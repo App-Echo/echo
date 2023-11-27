3fd7beadb036fc6cbf0788b0a422e23be7653bdd
Ale-Sandro <alessandromasson@gmail.com>
2023-11-27 22:15:34 +0100

    test(lint): check jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Echo App</h1>
+      <h1>Echo App - test</h1>
       {/* <Home /> */}
       {/* <Divider /> */}
       {/* <About /> */}
     </main>
-  )
+  );
 }
