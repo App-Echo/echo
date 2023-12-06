b6de358604bbfc57b1bb6d20681e874599bfed04
Ali <83013339+ali-hbl@users.noreply.github.com>
2023-12-06 16:10:31 +0100

    config(tailwind): add base config and styles (#6)
    * config(tailwind): add base config and styles
    
    * config(tailwind): modify config and set reference fontsize to 10px
    
    * config(tailwind): add size 0
    
    ---------
    
    Co-authored-by: Ale-Sandro <alessandromasson@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Echo App - test</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <h1 className="text-normal text-6xl tracking-tight text-light">Echo</h1>
       {/* <Home /> */}
       {/* <Divider /> */}
       {/* <About /> */}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,65 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    screens: {
+      sm: '48rem',
+      md: '64rem',
+      lg: '76rem',
+      xl: '96rem',
+      '2xl': '128rem',
+    },
+    spacing: {
+      '0': '0rem',
+      '1': '0.4rem',
+      '2': '0.8rem',
+      '3': '1.2rem',
+      '4': '1.6rem',
+      '5': '2.4rem',
+      '6': '3.2rem',
+      '7': '4.8rem',
+      '8': '5.4rem',
+      '9': '6rem',
+      '10': '6.4rem',
+    },
+    fontSize: {
+      xs: '1.2rem',
+      sm: '1.4rem',
+      base: '1.6rem',
+      lg: '1.8rem',
+      xl: '2rem',
+      '2xl': '2.4rem',
+      '3xl': '2.8rem',
+      '4xl': '3.2rem',
+      '5xl': '3.6rem',
+      '6xl': '4rem',
+    },
     extend: {
+      colors: {
+        mirage: {
+          '50': '#f4f6fb',
+          '100': '#e8ecf6',
+          '200': '#cbd8ec',
+          '300': '#9db6dc',
+          '400': '#6990c7',
+          '500': '#4672b1',
+          '600': '#345995',
+          '700': '#2b4779',
+          '800': '#273e65',
+          '900': '#253555',
+          '950': '#111827',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        diagonal: 'linear-gradient(45deg, #2b4779 0%, #253555 100%)',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
